3572db159de3d9d427f186e7774dccb56135a59a
stark-cupcake <manviucan@gmail.com>
2025-06-04 19:12:37 +0530

    Add files via upload

---
+++ courses.js
@@ -0,0 +1,14 @@
+ function showPage(index) {
+    window.location.href = index + '.html';
+}
+function showPage(gallery) {
+    window.location.href = gallery + '.html';
+}
+function showPage(kcjunior) {
+    window.location.href = kcjunior + '.html';
+}
+
+        function toggleMenu() {
+            const navLinks = document.querySelector('.nav-links');
+            navLinks.classList.toggle('active');
+        }
